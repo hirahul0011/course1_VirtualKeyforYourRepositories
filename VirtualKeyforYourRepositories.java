15cb907dc272362e1d776d217ef7b64b531c20bd
hirahul0011 <ra_10001@yahoo.co.in>
2023-06-19 16:38:50 +0530

    Option 2.b (Deleting the specified file from existing directory) added

--- VirtualKeyforYourRepositories.java
+++ VirtualKeyforYourRepositories.java
@@ -3,6 +3,10 @@ package course1_VirtualKeyforYourRepositories;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -86,9 +90,9 @@ public class VirtualKeyforYourRepositories {
         int  optionsI =  sc.nextInt();
         switch (optionsI){
         	case 1:
-        		System.out.println("Please enter the name for the file");
+        		System.out.println("Please enter the name for the file:");
         		String fileName =  sc.next();
-        		System.out.println("Please enter the info that you want to put in the file");
+        		System.out.println("Please enter the info that you want to put in the file:");
         		String data =  sc.next();
         		addAFileToDirectory(filesPath, fileName, data);
         		File fObj = new File(filesPath);                    	  
@@ -107,6 +111,22 @@ public class VirtualKeyforYourRepositories {
         		optionsInnerSelection();                
                 break;
         	case 2:
+        		System.out.println("Please enter file name that you want to delete:");
+        		String fileNameD =  sc.next();
+        		deleteSpecifiedFileFromDirectory(filesPath, fileNameD);
+        		System.out.println("Here is the list of remaining files:");
+        		File fObj1 = new File(filesPath);                    	  
+            	if(fObj1.exists() && fObj1.isDirectory())  
+            	{  
+                	// array for the files of the directory pointed by fObj  
+                	File a[] = fObj1.listFiles();  
+                	// display statements  
+                	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
+                	System.out.println("Displaying Files from the directory : " + fObj1);  
+                	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
+                	// Calling the method  
+                	printFileNames(a, 0, 0);  
+            	}
         		optionsInnerSelection();                
                 break;
         	case 3:
@@ -135,6 +155,27 @@ public class VirtualKeyforYourRepositories {
         FileOutputStream out = new FileOutputStream(filesPath+"\\"+fileName+".txt");
         out.write(data.getBytes());
         out.close();
+    }
+	
+	private static void deleteSpecifiedFileFromDirectory(String filesPath,String fileName) throws IOException
+    {        
+		try
+        { 
+            Files.delete(Paths.get(filesPath+"\\"+fileName+".txt")); 
+        } 
+        catch(NoSuchFileException e) //Not throwing NoSuchFileException
+        { 		        	
+            System.out.println("No such file exists"); 
+        } 
+        catch(DirectoryNotEmptyException e) 
+        { 
+            System.out.println("Directory is not empty."); 
+        } 
+        catch(IOException e) 
+        { 
+            System.out.println("Invalid permissions."); 
+        } 		          
+        System.out.println("Deletion successful.");
     }
 	
 	public static void printFileNames(File[] a, int i, int lvl)  

77206a179e30cfb5c61e852f8b62252c7bae4b8e
hirahul0011 <ra_10001@yahoo.co.in>
2023-06-19 16:56:30 +0530

    Displaying the files method added and some other little changes

--- VirtualKeyforYourRepositories.java
+++ VirtualKeyforYourRepositories.java
@@ -12,6 +12,8 @@ import java.util.Scanner;
 
 public class VirtualKeyforYourRepositories {
 
+	static String filesPath="H:\\Documents\\Study\\Simpli Learn\\PGP Program - CALTECH\\PG FSD Implement OOPS using JAVA with Data Structures and Beyond\\Assessment-VirtualKeyforYourRepositories\\File Folder";
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("\n**************************************\n");
@@ -25,7 +27,7 @@ public class VirtualKeyforYourRepositories {
                 "2. I wish to manage my files",
                 "3. Close the application"
         };
-        String filesPath="H:\\Documents\\Study\\Simpli Learn\\PGP Program - CALTECH\\PG FSD Implement OOPS using JAVA with Data Structures and Beyond\\Assessment-VirtualKeyforYourRepositories\\File Folder";
+        
 
         int  slen = arr.length;
         for(int i=0; i<slen;i++){
@@ -44,19 +46,7 @@ public class VirtualKeyforYourRepositories {
 
                 switch (options){
                     case 1:
-                    	File fObj = new File(filesPath);                    	  
-                    	if(fObj.exists() && fObj.isDirectory())  
-                    	{  
-	                    	// array for the files of the directory pointed by fObj  
-	                    	File a[] = fObj.listFiles();  
-	                    	// display statements  
-	                    	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
-	                    	System.out.println("Displaying Files from the directory : " + fObj);  
-	                    	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
-	                    	// Calling the method  
-	                    	printFileNames(a, 0, 0);  
-                    	}  
-                        
+                    	displayingTheFiles();
                         optionsSelection();
                         break;
                     case 2:
@@ -78,8 +68,7 @@ public class VirtualKeyforYourRepositories {
                 "2. Delete a specified file from the existing directory list",
                 "3. Search a user specified file from the directory",
                 "4. Return to the main menu"
-    	};
-		String filesPath="H:\\Documents\\Study\\Simpli Learn\\PGP Program - CALTECH\\PG FSD Implement OOPS using JAVA with Data Structures and Beyond\\Assessment-VirtualKeyforYourRepositories\\File Folder";
+    	};		
     	System.out.println("\nPlease select the operation that you want to perform on the files\t");
     	int slen = arr1.length;    	
         for(int i=0; i<slen;i++){
@@ -95,38 +84,14 @@ public class VirtualKeyforYourRepositories {
         		System.out.println("Please enter the info that you want to put in the file:");
         		String data =  sc.next();
         		addAFileToDirectory(filesPath, fileName, data);
-        		File fObj = new File(filesPath);                    	  
-            	if(fObj.exists() && fObj.isDirectory())  
-            	{  
-                	// array for the files of the directory pointed by fObj  
-                	File a[] = fObj.listFiles();  
-                	// display statements  
-                	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
-                	System.out.println("Displaying Files from the directory : " + fObj);  
-                	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
-                	// Calling the method  
-                	printFileNames(a, 0, 0);  
-            	}  
+        		displayingTheFiles();  
             	System.out.println("File added to the directory successfully");
         		optionsInnerSelection();                
                 break;
         	case 2:
         		System.out.println("Please enter file name that you want to delete:");
         		String fileNameD =  sc.next();
-        		deleteSpecifiedFileFromDirectory(filesPath, fileNameD);
-        		System.out.println("Here is the list of remaining files:");
-        		File fObj1 = new File(filesPath);                    	  
-            	if(fObj1.exists() && fObj1.isDirectory())  
-            	{  
-                	// array for the files of the directory pointed by fObj  
-                	File a[] = fObj1.listFiles();  
-                	// display statements  
-                	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
-                	System.out.println("Displaying Files from the directory : " + fObj1);  
-                	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
-                	// Calling the method  
-                	printFileNames(a, 0, 0);  
-            	}
+        		deleteSpecifiedFileFromDirectory(filesPath, fileNameD);        		
         		optionsInnerSelection();                
                 break;
         	case 3:
@@ -162,6 +127,9 @@ public class VirtualKeyforYourRepositories {
 		try
         { 
             Files.delete(Paths.get(filesPath+"\\"+fileName+".txt")); 
+            System.out.println("Deletion successful.");
+            System.out.println("Here is the list of remaining files:");
+    		displayingTheFiles();
         } 
         catch(NoSuchFileException e) //Not throwing NoSuchFileException
         { 		        	
@@ -174,8 +142,8 @@ public class VirtualKeyforYourRepositories {
         catch(IOException e) 
         { 
             System.out.println("Invalid permissions."); 
-        } 		          
-        System.out.println("Deletion successful.");
+        }	          
+        
     }
 	
 	public static void printFileNames(File[] a, int i, int lvl)  
@@ -190,6 +158,21 @@ public class VirtualKeyforYourRepositories {
 		}	  
 		printFileNames(a, i + 1, lvl);  
 	} 
+	
+	public static void displayingTheFiles() {
+		File fObj = new File(filesPath);                    	  
+    	if(fObj.exists() && fObj.isDirectory())  
+    	{  
+        	// array for the files of the directory pointed by fObj  
+        	File a[] = fObj.listFiles();  
+        	// display statements  
+        	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
+        	System.out.println("Displaying Files from the directory : " + fObj);  
+        	System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");  
+        	// Calling the method  
+        	printFileNames(a, 0, 0);  
+    	}		
+	}
 	
 	private static void closeApp() {
         System.out.println("Closing your application... \nThank you!");
